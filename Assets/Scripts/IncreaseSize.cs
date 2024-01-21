47eff46f0be44ebbf1717296e2e53c46e453260d
DIFIKYT <ragexses@mail.ru>
2024-01-21 21:59:23 +0700

    no message

--- Assets/Scripts/IncreaseSize.cs
+++ Assets/Scripts/IncreaseSize.cs
@@ -1,13 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class IncreaseSize : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    private float _scaleX = 1;
-    private float _scaleY = 1;
-    private float _scaleZ = 1;
 
     private void Update()
     {
@@ -16,10 +11,6 @@ public class IncreaseSize : MonoBehaviour
 
     private void Increase()
     {
-        _scaleX += _speed;
-        _scaleY += _speed;
-        _scaleZ += _speed;
-
-        transform.localScale = new Vector3(_scaleX, _scaleY, _scaleZ);
+        transform.localScale += new Vector3(_speed, _speed, _speed) * Time.deltaTime;
     }
 }
